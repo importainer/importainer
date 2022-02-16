ace321bb7ef0263e2421771f33bfdbc98e907388
Emanuel <emanuelgermano90@gmail.com>
2022-02-16 10:00:21 -0300

    prueba

--- src/components/NavBarPrinc/NavBarPrinc.js
+++ src/components/NavBarPrinc/NavBarPrinc.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+<<<<<<< HEAD
 import { Link } from 'react-router-dom';
 import LoginUser from "../LoginUser/LoginUser";
 import "./NavBarPrinc.css";
 import LogoImportainer from './../../images/Logo500.png';
+=======
+import { Link, HashRouter } from 'react-router-dom';
+import LoginUser from '../LoginUser/LoginUser';
+import LogoImportainer from './../../images/Logo500.png';
+import "./NavBarPrinc.css";
+>>>>>>> 73c5989 (conect emanuel)
 
 export default class NavBarPrinc extends Component {
   //Show - remove menu
@@ -14,6 +21,7 @@ export default class NavBarPrinc extends Component {
   render() {
     return ( 
       <div className="all">
+<<<<<<< HEAD
         <div className="menu-bar-p">
           <div className="logo-princ">
             <Link to="/">
@@ -35,6 +43,34 @@ export default class NavBarPrinc extends Component {
         <div className="menu-btn-p" id="menu-btn">
           <i className="fas fa-bars fa-2x" id="nav-icon" onClick={this.firstClick}></i>
         </div>
+=======
+
+        <HashRouter>
+
+          <div className="menu-bar-p">
+            <div className="logo-princ">
+              <Link to="/">
+                <img src={LogoImportainer} className="logo" alt="logo" />
+              </Link>
+            </div>
+
+            <nav className="nav-menu-p" id="nav-menu">
+              <ul className="nav-right-p">
+                <a href="#compañia" onClick={this.firstClick}>Compañias</a>
+                <Link to="../franquicias">Franquicias</Link>
+                <Link to="../proyectos" >Proyectos</Link>
+                <a href="#contacto" onClick={this.firstClick}>Contacto</a>
+              </ul>
+              <LoginUser />
+            </nav>
+          </div>
+
+          <div className="menu-btn-p" id="menu-btn">
+            <i className="fas fa-bars fa-2x" id="nav-icon" onClick={this.firstClick}></i>
+          </div>
+        
+        </HashRouter>
+>>>>>>> 73c5989 (conect emanuel)
       </div>
     );
   }
