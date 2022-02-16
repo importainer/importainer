6c1437ec587ab1646eb095d715d266e9db82edc0
Emanuel <emanuelgermano90@gmail.com>
2022-02-16 11:23:08 -0300

    actualizations

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-=======
 import { BrowserRouter as Router, Route, Switch, HashRouter } from "react-router-dom";
->>>>>>> 73c5989 (conect emanuel)
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
 import Franquicias from "../pages/Franquicias";
@@ -64,80 +60,6 @@ import UserDetail from "./Admin/UserDetail/UserDetail";
 
 import Plan from "./User/Plan/Plan";
 
-<<<<<<< HEAD
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-
-      {/* ===== ADMINISTACION ===== */}
-      <Route exact path="/admin" component={Login} />
-      <Route exact path="/indexAdm" component={IndexAdm} />
-      <Route exact path="/LoginCreate" component={LoginCreate} />
-      <Route exact path="/CreatePub" component={CreatePub} />
-      <Route exact path="/ListaPublicaciones" component={ListPub} />
-      <Route exact path="/EditarPub" component={EditarPub} />
-      <Route exact path="/users" component={LoginUser} />
-      <Route exact path="/UserDetail" component={UserDetail} />
-
-      {/* ===== USER ===== */}
-      <Route exact path="/DatoPlan" component={Plan} />
-
-      {/* ===== PROYECTOS ===== */}
-      <Route exact path="/proyectos" component={Proyectos} />
-
-      {/* ====== CASAS ====== */}
-      <Route exact path="/casas" component={Casas} />
-      <Route exact path="/casas/120M2" component={Cont120M2} />
-      <Route exact path="/casas/90M2" component={Cont90M2} />
-      <Route exact path="/casas/75M2" component={Cont75M2} />
-      <Route exact path="/casas/45M2" component={Cont45M2} />
-      <Route exact path="/casas/30M2" component={Cont30M2} />
-      <Route exact path="/casas/15M2" component={Cont15M2} />
-
-      {/* ====== EMPRENDIMIENTOS ===== */}
-      <Route exact path="/emprendimientos" component={Emprendimientos} />
-      <Route
-        exact
-        path="/emprendimientos/foodtrack15m2"
-        component={Foodtrack15M2}
-      />
-      <Route
-        exact
-        path="/emprendimientos/foodtrack30m2"
-        component={Foodtrack30M2}
-      />
-      <Route
-        exact
-        path="/emprendimientos/foodtrack120m2"
-        component={Foodtrack120M2}
-      />
-      <Route exact path="/emprendimientos/bar45m2" component={Bar45M2} />
-      <Route exact path="/emprendimientos/local90m2" component={Local90M2} />
-
-      {/* ===== EXTERIOR ===== */}
-      <Route exact path="/exterior" component={Exterior} />
-      <Route exact path="/exterior/quincho15m2" component={Quincho15M2} />
-      <Route exact path="/exterior/quincho30m2" component={Quincho30M2} />
-      <Route exact path="/exterior/pileta15m2" component={Pileta15M2} />
-      <Route exact path="/exterior/pileta30m2" component={Pileta30M2} />
-
-      {/* ===== OFICINAS ===== */}
-      <Route exact path="/oficinas" component={Oficinas} />
-      <Route exact path="/oficinas/oficina60m2" component={Oficina60M2} />
-
-      {/* ===== OTROS ====== */}
-      <Route exact path="/aspectos" component={Aspectos} />
-      <Route exact path="/franquicias" component={Franquicias} />
-      <Route exact path="/contacto" component={ContactoPage} />
-      <Route exact path="/reservas" component={Reservas} />
-
-      <Route exact path="/oferta" component={OfertaList} />
-      <Route exact path="/test" component={test} />
-      <Route component={NotFound} />
-    </Switch>
-  </Router>
-=======
 import NavBarPrinc from "./NavBarPrinc/NavBarPrinc";
 
 const App = () => (
@@ -219,7 +141,6 @@ const App = () => (
 
   </>
   
->>>>>>> 73c5989 (conect emanuel)
 );
 
 export default App;
--- src/components/NavBarPrinc/NavBarPrinc.js
+++ src/components/NavBarPrinc/NavBarPrinc.js
@@ -1,15 +1,8 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
-import { Link } from 'react-router-dom';
-import LoginUser from "../LoginUser/LoginUser";
-import "./NavBarPrinc.css";
-import LogoImportainer from './../../images/Logo500.png';
-=======
 import { Link, HashRouter } from 'react-router-dom';
 import LoginUser from '../LoginUser/LoginUser';
 import LogoImportainer from './../../images/Logo500.png';
 import "./NavBarPrinc.css";
->>>>>>> 73c5989 (conect emanuel)
 
 export default class NavBarPrinc extends Component {
   //Show - remove menu
@@ -21,29 +14,6 @@ export default class NavBarPrinc extends Component {
   render() {
     return ( 
       <div className="all">
-<<<<<<< HEAD
-        <div className="menu-bar-p">
-          <div className="logo-princ">
-            <Link to="/">
-              <img src={LogoImportainer} className="logo" alt="logo" />
-            </Link>
-          </div>
-
-          <nav className="nav-menu-p" id="nav-menu">
-            <ul className="nav-right-p">
-              <a href="#compañia" onClick={this.firstClick}>Compañias</a>
-              <Link to="../franquicias">Franquicias</Link>
-              <Link to="../proyectos" >Proyectos</Link>
-              <a href="#contacto" onClick={this.firstClick}>Contacto</a>
-            </ul>
-            {/* <LoginUser /> */}
-          </nav>
-        </div>
-
-        <div className="menu-btn-p" id="menu-btn">
-          <i className="fas fa-bars fa-2x" id="nav-icon" onClick={this.firstClick}></i>
-        </div>
-=======
 
         <HashRouter>
 
@@ -70,7 +40,6 @@ export default class NavBarPrinc extends Component {
           </div>
         
         </HashRouter>
->>>>>>> 73c5989 (conect emanuel)
       </div>
     );
   }
