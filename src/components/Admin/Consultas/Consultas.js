3001cfbbfdfbf71c3393b5dc29e8fc43c261dd15
Emanuel <emanuelgermano90@gmail.com>
2022-03-09 11:39:33 -0300

    component consultasUser

--- src/components/Admin/Consultas/Consultas.js
+++ src/components/Admin/Consultas/Consultas.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../../../firebase";
 import { getDocs, collection } from "firebase/firestore";
+import ArrowRightIcon from '@mui/icons-material/ArrowRight';
+import ArrowLeftIcon from '@mui/icons-material/ArrowLeft';
 import NavBar from "../NavBar/NavBar";
 import ConsultasCard from "../ConsultasCard/ConsultasCard";
 import Consulta from "./Consultas.module.css";
@@ -9,12 +11,42 @@ export default function Consultas({location}) {
 
     const [conUsers, setConUsers] = useState([]);
 
+    const [paginationIz, setPaginationIz] = useState(0);
+
+    const [paginationDer, setPaginationDer] = useState(10);
+
     useEffect(() => {
 
         getDocs(collection(db, "users"))
             .then(e => {
 
-                setConUsers(e.docs.map(e => e.data()));
+                const consultas = e.docs.filter(e => e.data().userObject.fecha !== undefined);
+                    
+                setConUsers(consultas.map(e => {
+
+                    const arr = e.data().userObject.fecha.split('/');
+
+                    const dia = arr[0];
+
+                    const mes = arr[1];
+
+                    const año = arr[2];
+
+                    const hoy = new Date(año,mes,dia);
+                    
+                    return {
+
+                        id: e.id,
+                        email: e.data().userObject.email,
+                        fecha: hoy.toISOString(),
+                        message: e.data().userObject.message,
+                        name: e.data().userObject.name,
+                        phone: e.data().userObject.phone,
+                        usado: e.data().userObject.usado,
+
+                    }
+
+                }));
 
             })
             .catch(e => console.log(e, 'error'))
@@ -23,8 +55,13 @@ export default function Consultas({location}) {
 
     console.log(conUsers, 'users');
 
-    const arr = [{ email: 'prueba@pruega.com', name: 'prueba', phone: '3215648',
-                    usado: true, message: 'quiero saber los precios de la casita de campanita', fecha: '12/02/2022'}]
+    const ordenarFecha = (a, b) => {
+
+        if(a > b) return -1
+        else if(a < b) return 1
+        else return 0
+
+    }
 
     return (
 
@@ -34,14 +71,32 @@ export default function Consultas({location}) {
 
             <h1>Consultas de Usuarios</h1>
 
+            <div className={Consulta.arrowContent}>
+
+                <div className={Consulta.arrowLeft} >
+
+                    <ArrowLeftIcon sx={{ fontSize: 100, color: '#FF0000', margin: '0px', padding: '0px' }} />   
+                    
+                </div>
+
+                <div>
+
+                    <ArrowRightIcon sx={{ fontSize: 100, color: '#FF0000' }} />
+                    
+                </div>   
+
+            </div>
+
             {
 
-                arr.map(e => {
+                conUsers.sort((a, b) => ordenarFecha(a.fecha, b.fecha)).slice(paginationIz, paginationDer).map((e, i) => {
 
+                    const fechaMap = new Date(e.fecha);
+                    
                     return (
 
-                        <ConsultasCard email={e.email} name={e.name} phone={e.phone} 
-                            usado={e.usado} message={e.message} fecha={e.fecha} />
+                        <ConsultasCard id={e.id} email={e.email} name={e.name} phone={e.phone} 
+                            usado={e.usado} message={e.message} fecha={`${fechaMap.getDate()}/${fechaMap.getMonth()}/${fechaMap.getFullYear()}`} />
 
                     )
 
--- src/components/Admin/ConsultasCard/ConsultasCard.js
+++ src/components/Admin/ConsultasCard/ConsultasCard.js
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
+import { app } from "../../../firebase";
 import ExpandLessIcon from '@mui/icons-material/ExpandLess';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import Switch from '@mui/material/Switch';
+import { alpha, styled } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
 import "../ConsultasCard/ConsultasCard.css";
 
 export default function ConsultasCard({ id, email, name, phone, usado, message, fecha}) {
+    // id = 1
 
+    const [consultas, setConsultas] = useState({
+
+        email, name, phone, usado, message, fecha
+
+    })
+    // 
     const [active, setActive] = useState(false);
+
+    const [checked, setChecked] = useState(usado);
+
+    const [checked2, setChecked2] = useState(usado);
 
     const deslizContent = () => {
 
         const cont = document.getElementById(id);
-
+        
         if(!active) {
 
-            cont.className = "reservaCard__contentAction";
+            cont.className = "ConsultaCard__contentAction";
 
             setActive(true);
 
         } else {
 
-            cont.className = "reservaCard__contentDisable";
+            cont.className = "consultaCard__contentDisable";
             
             setTimeout(() => {
 
-                cont.className = "reservaCard__content";
+                cont.className = "ConsultaCard__content";
 
             }, 1000);
 
@@ -33,16 +48,66 @@ export default function ConsultasCard({ id, email, name, phone, usado, message, 
 
     }
 
-    console.log( email, name, phone, usado, message, fecha, 'datos')
+    const GreenSwitch = styled(Switch)(({ theme }) => ({
+
+        '& .MuiSwitch-switchBase.Mui-checked': {
+          color: red[500],
+          '&:hover': {
+            backgroundColor: alpha(red[500], theme.palette.action.hoverOpacity),
+          },
+        },
+        '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+          backgroundColor: red[500],
+        },
+
+    }));
+
+    const label = { inputProps: { 'aria-label': 'Switch demo' } };
+    
+    const actualizarDB = async (obj) => {
+        
+        const collectionRef = await app.firestore().collection("users").doc(id);
+        
+        await collectionRef.update(obj);     
+        
+        collectionRef.update(obj);
+
+    }
+
+    const handleChange = () => {
+        
+        checked ? setChecked(false) : setChecked(true);
+
+    } 
+
+    if(checked !== checked2) {
+        
+        const userObject = {...consultas, usado: checked}
+        
+        actualizarDB({userObject});
+
+        setChecked2(checked);
+
+    }
+
+    // console.log( email, name, phone, usado, message, fecha, 'datos')
     return (
 
         <div className='ConsultasContent' >
 
-            <h1>{name}</h1>
+            <div className='contentHeat' >
 
-            <h3 className='fecha' >{fecha}</h3>
+                <h1>{name}</h1>
 
-            <h4>{usado}</h4>
+                <h3 className='fecha' >{fecha}</h3>
+
+                <div className="titleDetail__Switch">
+
+                    <GreenSwitch {...label} checked={checked} onChange={handleChange} defaultChecked />
+
+                </div>
+
+            </div>
 
             <div className={`consultasCard__icon`} >
                     
@@ -57,11 +122,15 @@ export default function ConsultasCard({ id, email, name, phone, usado, message, 
 
             <div id={id} className='ConsultaCard__content' >
 
-                <h4 className='email' >{email}</h4>
+                <h4 className='ConsultaCard__contentIzq' >{email}</h4>
 
-                <h4 className='phone' >{phone}</h4>
+                <h4 className='ConsultaCard__contentDer' >{phone}</h4>
 
-                <p>{message}</p>
+                <div className='ConsultaCard__contentMess' >
+
+                    <p>{message}</p>
+
+                </div>
 
             </div>
         
