cfc4fad2406542d8fe50d288a1441c85a398e1ce
Emanuel <emanuelgermano90@gmail.com>
2022-03-17 14:52:02 -0300

    siguiente

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -11,32 +11,39 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
 
     const [paginasIzq, setPaginasIzq] = useState(0);
 
-    let activeDiv = document.getElementsByClassName(Pag.active)[0];
-
-    // setTimeout(() => activeDiv = document.getElementsByClassName(Pag.active)[0], 50);
-    
     const active = (e) => {
 
         const { id } = e.target;
-        console.log(id, 'id')
+
+        const activeDiv = document.getElementsByClassName(Pag.active)[0];
+        
         // quitamos el estilo activ al div anterior
         activeDiv.className = Pag.num;
         
          // agregamos estilo activ al div actual
-         const actDiv = document.getElementById(id);
+        const actDiv = document.getElementById(id);
 
-         actDiv.className = Pag.active;
+        actDiv.className = Pag.active;
 
          pagSelect(id)
 
     }
 
     const nextBackPaginado = (x) => {
+        
+        const activeDiv = document.getElementsByClassName(Pag.active)[0];
 
         const act = x === 1 ? (parseInt(activeDiv?.id) + 1) : (parseInt(activeDiv?.id) - 1);
-        console.log(act, 'ver')
+        
+        if(act > count) {
 
-        if(act >= (count - 2)) {
+            // inicio
+
+            setPaginasDer(5);
+
+            setPaginasIzq(0);
+
+        } else if(act >= (count - 2) || act === 0) {
 
             // final
 
@@ -52,14 +59,6 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
 
             setPaginasIzq(act - 3);
 
-        } else if(act === 0) {
-
-            // atras del 0
-
-            setPaginasDer((count + 1));
-
-            setPaginasIzq((count - 5));
-
         } else {
 
             // inicio
@@ -70,17 +69,20 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
 
         }
 
-        setRecorrido('2da');
+        
 
     }
 
     const back = () => {
-        console.log(activeDiv, 'ver')
 
+        const activeDiv = document.getElementsByClassName(Pag.active)[0];
+        
         nextBackPaginado(0);
 
-        const ant = parseInt(activeDiv.id) - 1;
+        setRecorrido('2da');
 
+        const ant = parseInt(activeDiv.id) - 1;
+        
         activeDiv.className = Pag.num;
         
         if(ant === 0) {
@@ -98,9 +100,9 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
             
             // agregamos estilo activ al div actual
             const actDiv = document.getElementById(ant);
-
+            
             actDiv.className = Pag.active;
-
+            
         }
         
         anterior();
@@ -108,16 +110,22 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
     }
 
     const next = () => {
+        
+        const activeDiv = document.getElementsByClassName(Pag.active)[0];
+        console.log(activeDiv, 'next');
+        nextBackPaginado(1);
+
+        setRecorrido('2da');
 
         const sig = parseInt(activeDiv.id) + 1;
 
         activeDiv.className = Pag.num;
 
         if(sig > count) {
-            
+            console.log(document.getElementById('1'), 'next 1ro')
             // agregamos estilo activ al div actual
             const actDiv = document.getElementById('1');
-
+            
             actDiv.className = Pag.active;
 
         } else {
@@ -131,14 +139,14 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
         
         siguiente();
 
-        nextBackPaginado(1);
-
     }
 
     const cls = (e) => {
         
+        const activeDiv = document.getElementsByClassName(Pag.active)[0];
+        console.log('1')
         if(recorrido === true){
-
+            console.log('2')
             const d = document.getElementById(e);
 
             e < 2 ? d.className = Pag?.active: d.className = Pag?.num;
@@ -146,10 +154,10 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
             setRecorrido(false);
 
         } else if(recorrido === '2da') {
-            
+            console.log('3')
             const d = document?.getElementById(e);
 
-            e === parseInt(activeDiv.id) ? d.className = Pag?.active: d.className = Pag?.num;
+            e === parseInt(activeDiv?.id) ? d.className = Pag?.active: d.className = Pag?.num;
 
             setRecorrido(false);
 
@@ -209,7 +217,7 @@ export default function Pagination({ count, pagSelect, anterior, siguiente }) {
         )
 
     }
-
+    
     return (
 
         <>
